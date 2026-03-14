47a9fb6a3e0218cc8ba242e5904eeb8321189576
ArrushiTripathi2429 <arrushi111@gmail.com>
2026-03-14 18:02:03 +0530

    fix: skip dotenv in production

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const express = require('express');
 const cors = require('cors');
@@ -21,6 +23,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+
 
 
 
